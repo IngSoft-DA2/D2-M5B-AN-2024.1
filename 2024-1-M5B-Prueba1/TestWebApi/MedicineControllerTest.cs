58bf70bbf8776c87e7c7d979e163ccb316805eea
Natalia-Leites <63421733+Natalia-Leites@users.noreply.github.com>
2024-05-08 09:59:57 -0300

    prueba-1-tec

---
+++ 2024-1-M5B-Prueba1/TestWebApi/MedicineControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using WebAPI;
+using Drugs;
+using Medicine;
+using Logic;
+using Moq;
+
+namespace TestWebApi
+{
+    [TestClass]
+    public class MedicineControllerTest
+    {
+        private Mock<IMedicineService>? _medicineServiceMock;
+        private MedicineController? _controller;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _medicineServiceMock = new Mock<IMedicineService>(MockBehavior.Strict);
+            _drugsServiceMock = new Mock<IDrugsService>(MockBehavior.Strict);
+            _controller = new MedicineController(MedicineController.Object);
+        }
+
+        [TestMethod]
+        public void MainDrug_ShouldReturnCorrectDrug()
+        {
+            //string GetMainDrug(Medicine medicine);
+            Drug mainDrug = drugs[0];
+            
+            _drugsServiceMock.Setup(x => x.GetMainDrug(It.IsAny<mainDrug>())).Returns(GetMainDrug);
+            _controller.GetMainDrug(mainDrug);
+        }
+
+
+        [TestMethod]
+        public void GetAllMedicine_ShouldReturnCorrectMedicineList()
+        {
+            //IEnumerable<Medicine> GetAll();
+            var medicines = new List<Medicine>
+            {
+                new Medicine() { Name = "Dolex 500", Drugs = [new Drug() { Milligrams = 500, Name = "paracetamol"}]},
+                new Medicine() { Name = "Perifar 200", Drugs = [new Drug() { Milligrams = 200, Name = "ibuprofeno"}]},
+           };
+            _drugsServiceMock.Setup(x => x.GetAll(It.IsAny<medicines>())).Returns(medicines);
+            _controller.GetAll();
+        }
+
+
+    }
+}
