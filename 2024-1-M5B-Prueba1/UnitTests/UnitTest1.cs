254246afb92bc4311824d449868ed2bedbcfb0e6
Felipe <lussichfelipe173@gmail.com>
2024-05-08 09:49:19 -0300

    final

--- 2024-1-M5B-Prueba1/UnitTests/UnitTest1.cs
+++ 2024-1-M5B-Prueba1/UnitTests/UnitTest1.cs
@@ -5,6 +5,7 @@ using Logic;
 using Logic.Models;
 using Drugs;
 using Medicines;
+using WebAPI.Controllers;
 
 
 [TestClass]
@@ -31,10 +32,21 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [ExpectedException(typeof(Exception))]
-    public void GetMainDrugWithMultipleDrugsTest()
+    public void GetAllTest()
     {
-    
+        var medicineService = new MedicineService(new Mock<IDrugsService>().Object);
+        var medicineController = new MedicineController(medicineService);
+        var result = medicineController.GetAll();
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    public void MainDrugTest()
+    {
+        var medicineService = new MedicineService(new Mock<IDrugsService>().Object);
+        var medicineController = new MedicineController(medicineService);
+        var result = medicineController.MainDrug(new Medicine() { Name = "Dolex 500", Drugs = [new Drug() { Milligrams = 500, Name = "paracetamol"}]});
+        Assert.IsNotNull(result);
     }
 
 }
